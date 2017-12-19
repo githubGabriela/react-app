64c2c049b8a1f0a2a3457e3208913ea87a2ed86f
Gabriela <gabriela_balasel@yahoo.com>
2017-12-19 16:42:31 +0100

    (refactor) moved accordion logic to Products

--- src/components/categories/CategoryWithProducts.js
+++ src/components/categories/CategoryWithProducts.js
@@ -1,46 +1,27 @@
 import React, { Component } from 'react';
 import '../../assets/css/General.css';
 
+
 import ProductItem from '../products/ProductItem';
 
 class CategoryWithProducts extends Component {
-    defaultHeight = '80px';
-    reducedHeight = '40px';
-
-    constructor(props){
-        super(props);
-        this.state = {
-            showProducts: false,
-            categoryStyle: {
-                backgroundColor: this.props.category.color,
-                height: this.defaultHeight
-            }
-        }
-        this.toggleProducts = this.toggleProducts.bind(this);
-    }
-
-    toggleProducts(){
-        let toggledProducts = !this.state.showProducts;
-        this.setState({
-            showProducts: toggledProducts,
-            categoryStyle: {
-                backgroundColor: this.props.category.color,
-                height: toggledProducts ? this.reducedHeight : this.defaultHeight
-            }
-        })
-    }
 
     render() {
+        let categoryStyle = {
+            backgroundColor: this.props.category.color,
+            height: this.props.categoryHeight
+        }
         return (
             <div>
-                    <div className="category"
-                         style={this.state.categoryStyle} onClick={this.toggleProducts}> 
+                <div>{this.props.showSection}</div>
+                <div className="category"
+                         style={categoryStyle}> 
                             {this.props.category.label}
                          </div>
                     {this.props.category.products.map(product => {
                         return ( 
-                            <div> 
-                                { this.state.showProducts ? 
+                            <div key={product.key}> 
+                                { this.props.showSection ? 
                                 <ProductItem key={product.key} product={product} 
                                             color={this.props.category.color}/>
                                 : <span></span>
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -7,12 +7,42 @@ import { hocCategories } from '../categories/HocCategories';
 
 
 class AllProducts extends Component {
+    defaultHeight = '80px';
+    reducedHeight = '40px';
+
+    constructor(props){
+        super(props);
+        this.state = {
+            showSection : {},
+            categoryHeight: this.defaultHeight
+        }
+        this.toggleSection = this.toggleSection.bind(this);
+    }
+
+    toggleSection(key){
+        let toggle =  {
+            key: key,
+            show: this.state.showSection[key] ? this.state.showSection[key].show : false
+        }
+        this.setState({
+            showSection : toggle,
+            categoryHeight: this.state.showSection.show ? this.defaultHeight : this.reducedHeight
+        });
+
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div>
               {this.props.categories.map((category) => {
-                  return <CategoryWithProducts key={category.key} category={category}/>
-              })}
+                  return <div key={category.key} onClick={()=> this.toggleSection(category.key)}>
+                            <CategoryWithProducts 
+                                               categoryHeight={this.state.categoryHeight}
+                                               showSection={this.state.showSection.show}
+                                               category={category}/>
+                      </div>
+             })}
             </div>
         )
     }
