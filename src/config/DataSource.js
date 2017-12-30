93ba9d400073d427c138c09d8a5f8c087e8c284d
Gabriela <gabriela_balasel@yahoo.com>
2017-12-30 23:24:56 +0100

    (refactor) refactor DataSource file

--- src/config/DataSource.js
+++ src/config/DataSource.js
@@ -1,14 +1,16 @@
 import { dbDataCategories, dbDataProducts, dbDataShoppingList, dbDataHistory } from './constants';
 
-//Categories
-export function getCategories(customFct) { // customFct - function called in the component
-    dbDataCategories.on('value', (snap) => {
-        const items = [];
-        snap.forEach( childSnap => {
-            items.push({ key: childSnap.key, value: childSnap.val()});
-        });
-        customFct(items);
+//GET
+function getKeyValues(snap, customFct) { // customFct - function called in the component
+    const items = [];
+    snap.forEach( childSnap => {
+        items.push({ key: childSnap.key, value: childSnap.val()});
     });
+    customFct(items);
+}
+
+export function getCategories(customFct) { 
+    dbDataCategories.on('value', snap => getKeyValues(snap, customFct));
 }
 
 export function getCategoriesNames(customFct){
@@ -31,12 +33,6 @@ export function getCategoriesForDropdown(customFct) {
     });
 }
 
-export function addCategory(category) {
-    if(category && category.name){
-        dbDataCategories.push(category);
-    }
-}
-
 export function getColorForCategory(categoryName, customFct){
     dbDataCategories.orderByChild('name').equalTo(categoryName).on('value', snap => {
         let color= '';
@@ -47,58 +43,33 @@ export function getColorForCategory(categoryName, customFct){
     });
 }
 
-
-// Products
 export function getProducts(customFct) {
-    dbDataProducts.on('value', snap => {
-        const items = [];
-        snap.forEach( childSnap => {
-            items.push({ key: childSnap.key, value: childSnap.val()});
-        });
-        customFct(items);
-    });
+    dbDataProducts.on('value', snap => getKeyValues(snap, customFct));
 }
 
 export function getProductsByCategory(customFct){
-    dbDataProducts.orderByChild('category').on('value', snap => {
-        let items = [];
-        snap.forEach(childSnap => {
-            items.push({key: childSnap.key, value: childSnap.val()});
-        });
-        customFct(items);
-    });
+    dbDataProducts.orderByChild('category').on('value', snap => getKeyValues(snap, customFct));
+}
+
+export function getShoppingList(customFct){
+    dbDataShoppingList.orderByChild('category').on('value', snap => getKeyValues(snap, customFct));
+}
+
+export function getHistory(customFct){
+    dbDataHistory.orderByChild('category').on('value', snap => getKeyValues(snap, customFct));
+}
+
+// PUSH
+export function addCategory(category) {
+    if(category && category.name){
+        dbDataCategories.push(category);
+    }
 }
 
 export function addProduct(product){
     if(product && product.name){
         dbDataProducts.push(product);
     } 
-}
-
-
-// Products & Categories
-export function update(dbDataType, key, value){
-    dbDataType.child(key).update(value);
-}
-
-export function removeFromDb(items, dbDataType){
-    items.forEach( item => {
-        if(item.key){
-            dbDataType.child(item.key).remove();
-        }
-    })
-}
-
-
-// ShoppingList
-export function getShoppingList(customFct){
-    dbDataShoppingList.orderByChild('category').on('value', snap => {
-        let items = [];
-        snap.forEach(childSnap => {
-            items.push({key: childSnap.key, value: childSnap.val()});
-        });
-        customFct(items);
-    });
 }
 
 export function addToShoppingList(item){
@@ -112,31 +83,36 @@ export function addToShoppingList(item){
     }
 }
 
-export function removeFromShoppingList(item){
-    if(item && item.key) {
-       dbDataShoppingList.child(item.key).remove();
-    }
-}
-
-// History
-export function getHistory(customFct){
-    dbDataHistory.orderByChild('category').on('value', snap => {
-        let items = [];
-        snap.forEach(childSnap => {
-            items.push({key: childSnap.key, value: childSnap.val()});
-        });
-        customFct(items);
-    });
-}
-
 export function addToHistory(item){
     if(item && item.value) {
           dbDataHistory.push(item.value);
      }
 }
 
+
+// UPDATE
+export function update(dbDataType, key, value){
+    dbDataType.child(key).update(value);
+}
+
+
+// REMOVE
+export function removeFromDb(items, dbDataType){
+    items.forEach( item => {
+        if(item.key){
+            dbDataType.child(item.key).remove();
+        }
+    })
+}
+
+export function removeFromShoppingList(item){
+    if(item && item.key) {
+       dbDataShoppingList.child(item.key).remove();
+    }
+}
+
 export function removeAllHistory(items){
     items.forEach( item => {
         dbDataHistory.child(item.key).remove();
-    })
+    });
 }
