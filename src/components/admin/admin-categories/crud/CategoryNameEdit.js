a80795235cc3cd6d0a362c32952a13478dd7ce38
Gabriela <gabriela_balasel@yahoo.com>
2018-01-05 14:37:22 +0100

    (refactor) cleanups

--- src/components/admin/admin-categories/crud/CategoryNameEdit.js
+++ src/components/admin/admin-categories/crud/CategoryNameEdit.js
@@ -12,29 +12,19 @@ class CategoryNameEdit extends Component {
  
     constructor(props){
         super(props);
- 
          this.state = {
              item : props.item,
+             errorMessage: '',
              isEditable : false
          };
  
-        this.edit = this.edit.bind(this);
         this.inputChange = this.inputChange.bind(this);
-        this.resetItem = this.resetItem.bind(this);
         this.setReadOnly = this.setReadOnly.bind(this);
-        this.confirm = this.confirm.bind(this);
-     }
- 
-     edit(event){
-        Utils.preventDefault(event);
-         this.setState({
-             isEditable : true
-         });
      }
  
      inputChange(event){
          let updatedName = event.target.value;
-         if(updatedName !== this.state.item.value.name && this.isValidLabel(this.state.item.value.name)){
+         if(updatedName !== this.state.item.value.name){
              this.setState({
                  item : {
                      key : this.state.item.key,
@@ -46,58 +36,72 @@ class CategoryNameEdit extends Component {
                  }
              });
          }
+         this.clearError();
      }
 
-    resetItem(event) {
-        Utils.preventDefault(event);
-         this.setState({
-             item : this.props.item
-         });
-         this.setReadOnly();
-     }
- 
-     setReadOnly(){
-         this.setState({
-             isEditable: false
-         });
-     }
- 
-     isValidLabel(label){
-         return label && label.trim().length > 0;
-     }
- 
-
-    confirm(event) {
-        Utils.preventDefault(event);
+     confirm() {        
         let key = this.props.item.key;
-        if(key){
+        if(key && Utils.isValidValue(this.state.item.value.name)) {
             let value = {name: this.state.item.value.name};
-            DataSource.updateCategory(key, value, error => {console.log('error update category', error);});
+            DataSource.updateCategory(key, value, error => this.setError(error));
         }
     }
+  
+    setError(error) {
+        if(error && error.message){
+            this.setState({
+                errorMessage : error.message
+            });
+            
+        } else {
+            this.setReadOnly();
+        }
+    }
+    
+    clearError() {
+        this.setState({
+                errorMessage : ''
+        });
+    }
 
+    setReadOnly() {
+        this.setState({
+            isEditable: false
+        });
+    }
+    
     render() {
         return (
             <div className="item-edit">
             {!this.state.isEditable ? 
                 <div className="center-from-top flex space-between">
-                        <label>{this.state.item.value.name}</label>
-                        <FontAwesome name="pencil" onClick={this.edit}/>
+                    <label>{this.state.item.value.name}</label>
+                    <FontAwesome name="pencil" onClick={(event) => {
+                                                        Utils.preventDefault(event); 
+                                                        this.setState({isEditable : true})
+                                                        }}/>
                 </div>
             : 
                 <div className="center-from-top-input flex space-between">
                         <input type="text" className="input-text" value={this.state.item.value.name} onChange={this.inputChange}/>
                         <div className="edit-icons">
-                            <span onClick={this.setReadOnly}>
-                                <FontAwesome name="check" className="icon-with-padding" onClick={this.confirm}/>
-                            </span>
-                            <FontAwesome name="close" className="icon-with-padding" onClick={this.resetItem}/>
+                            <FontAwesome name="check" className="icon-with-padding" 
+                                                      onClick={(event) => {
+                                                                Utils.preventDefault(event);
+                                                                this.confirm();
+                                                                }}/>
+                            <FontAwesome name="close" className="icon-with-padding" 
+                                                      onClick={(event) => {
+                                                                Utils.preventDefault(event);
+                                                                this.setState({item : this.props.item});
+                                                                this.setReadOnly();
+                                                                this.clearError();
+                                                      }}/>
                         </div>
+                    <div className="red">{this.state.errorMessage}</div>
                 </div>
             }
         </div>
-
-           
         );
     }
 }
